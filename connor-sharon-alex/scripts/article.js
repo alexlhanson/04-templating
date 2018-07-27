116ff38058b490689badc625432360cee44e529f
Connor <cacrossley22@gmail.com>
2018-07-27 08:46:17 -0700

    refactor Article constructor

--- connor-sharon-alex/scripts/article.js
+++ connor-sharon-alex/scripts/article.js
@@ -3,16 +3,22 @@
 let articles = [];
 
 function Article (rawDataObj) {
-  this.author = rawDataObj.author;
-  this.authorUrl = rawDataObj.authorUrl;
-  this.title = rawDataObj.title;
-  this.category = rawDataObj.category;
-  this.body = rawDataObj.body;
-  this.publishedOn = rawDataObj.publishedOn;
+  // this.author = rawDataObj.author;
+  // this.authorUrl = rawDataObj.authorUrl;
+  // this.title = rawDataObj.title;
+  // this.category = rawDataObj.category;
+  // this.body = rawDataObj.body;
+  // this.publishedOn = rawDataObj.publishedOn;
+
+  // BONUS POINTS FOR US!
+  for (let key in rawDataObj) {
+    this[key] = rawDataObj[key];
+  }
 }
 
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
